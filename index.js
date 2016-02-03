2abb79687ea9a5ee8eecaa4049fb878c9faf1af0
Julian Giuca <julian.giuca@gmail.com>
2016-02-02 21:00:58 -0800

    simplify the recording - the source has changed

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
 'use strict';
 var request = require('request');
 var fs      = require('fs');
-var net = require('net');
 
 var second  = 1000;
 var minute  = second * 60;
 var hour    = minute * 60;
-var timeout = 2*hour;
+var timeout = 1.45*hour;
 
-var url         = 'http://shoutmedia.abc.net.au:10426/';
-var destination = '/volume1/music/music/radio/morning.mp3';
+var url         = 'http://live-radio01.mediahubaustralia.com/2TJW/aac/'
+var destination = '/volume1/music/music/radio/morning.aac';
 var file = fs.createWriteStream(destination);
 
-var msg = 'GET / HTTP/1.1\r\n' +
-          'User-Agent: node\r\n' +
-          'Host: http://shoutmedia.abc.net.au/\r\n' +
-          'Accept: */*\r\n\r\n';
-
-var client = net.connect(10426, 'shoutmedia.abc.net.au', function() {
-  console.log('connected to the server');
-  client.write(msg);
-});
-
-client.on('data', function (data) {
-  file.write(data);
-})
+request(url).pipe(fs.createWriteStream(destination))
 
 setTimeout(function () {
   file.close(function () {
